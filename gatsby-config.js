1cccfad2f5a9028471a22da198d42195de2582b8
tyj144 <tylerj81@gmail.com>
2018-09-20 18:41:46 -0400

    Added Google Analytics to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-116015171-1`,
       },
     },
     `gatsby-plugin-feed`,
