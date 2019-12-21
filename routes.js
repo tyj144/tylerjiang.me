f786137f171cf03c788485c311236b3111023279
Tyler Jiang <tylerj81@gmail.com>
2019-12-20 19:48:03 -0500

    Deprecated unused components

--- routes.js
+++ routes.js
@@ -11,12 +11,8 @@ module.exports = {
       component: path.resolve('./src/components/Blog/Blog.js'),
     },
     {
-      path: '/resume',
-      component: path.resolve('./src/components/Resume/Resume.js'),
-    },
-    {
       path: '/projects',
       component: path.resolve('./src/components/Projects/Projects.js'),
     },
-  ]
+  ],
 }
